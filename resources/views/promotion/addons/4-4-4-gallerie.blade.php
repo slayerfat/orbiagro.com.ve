21a42b773421e583cf27b1fa1604aa6894f53641
slayerfat <slayerfat@gmail.com>
2015-05-21 11:32:06 -0430

    ajustado por ahora imagen de 4-4-4 promo y navbar productos

--- resources/views/promotion/addons/4-4-4-gallerie.blade.php
+++ resources/views/promotion/addons/4-4-4-gallerie.blade.php
@@ -5,20 +5,20 @@
       <!-- elimina first child -->
       <div class="media"><div class="media-body"></div></div>
       <!-- importante -->
-        @foreach($promotions as $promo)
-          <div class="media col-md-4">
-            <a class="pull-left" href="#">
-              <img class="media-object" src="{{ $promo->images->first()->path }}" alt="{{ $promo->slug }}">
-            </a>
-            <div class="media-body">
-              <h4 class="media-heading">{{$promo->title}}</h4>
-              {{$promo->description}}
-              <p>
-                <a href="#">Mas Información</a>
-              </p>
-            </div>
+      @foreach($promotions as $promo)
+        <div class="media col-md-4">
+          <a class="pull-left" href="#">
+            <img class="media-object" src="{{ $promo->images->first()->path }}" alt="{{ $promo->slug }}" style="max-height:150px;">
+          </a>
+          <div class="media-body">
+            <h4 class="media-heading">{{$promo->title}}</h4>
+            {{$promo->description}}
+            <p>
+              <a href="#">Mas Información</a>
+            </p>
           </div>
-        @endforeach
+        </div>
+      @endforeach
     </div>
   </div>
 </div>
