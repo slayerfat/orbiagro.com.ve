53c53ebc9036963e7f7926d223367089c0ab908d
slayerfat <slayerfat@gmail.com>
2015-06-16 14:00:45 -0430

    ajustando user show

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -5,7 +5,7 @@
 @stop
 
 @section('content')
-  @if(Auth::user() and Auth::user()->isAdmin())
+  {{-- @if(Auth::user() and Auth::user()->isAdmin())
     <div class="container">
       <div class="row">
         <div class="col-xs-2">
@@ -13,9 +13,28 @@
         </div>
       </div>
     </div>
-  @endif
-  <div class="container">
+  @endif --}}
+  <div class="container-fluid">
     <div class="row">
+      <div class="col-sm-2 sidebar">
+        <ul class="nav nav-sidebar">
+          <li class="active">
+            <a href="#">Resumen</a>
+          </li>
+          <li>
+            <a href="#">Información Personal</a>
+          </li>
+          <li>
+            <a href="#">Seguridad</a>
+          </li>
+          <li>
+            <a href="#">Productos</a>
+          </li>
+          <li>
+            <a href="#">Facturación</a>
+          </li>
+        </ul>
+      </div>
       {{-- user info --}}
       <div class="col-sm-4">
         <h1>
@@ -42,7 +61,7 @@
       </div>
       {{-- productos --}}
       @unless($products->isEmpty())
-        <div class="col-sm-8">
+        <div class="col-sm-6">
           <h1>Productos</h1>
           @include('partials.products.paginated', ['products' => $products])
         </div>
