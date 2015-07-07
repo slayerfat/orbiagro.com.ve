78bbd258268a8d0021c2ed684c2d3ea744413e9c
slayerfat <slayerfat@gmail.com>
2015-07-07 13:21:35 -0430

    carpinteria

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -79,6 +79,7 @@
           <li><a href="/auth/login">Entrar</a></li>
           <li><a href="/auth/register">Registrarse</a></li>
         @else
+          <p class="navbar-text">Hola {{Auth::user()->name}}!</p>
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->username }} <span class="caret"></span></a>
             <ul class="dropdown-menu" role="menu">
