a68c2fc66124aeeaf2be54a034e0d056a0e14cd8
slayerfat <slayerfat@gmail.com>
2015-07-07 13:56:10 -0430

    ajustando generacion de confirmacion

--- app/Http/Middleware/RedirectIfUnverified.php
+++ app/Http/Middleware/RedirectIfUnverified.php
@@ -16,8 +16,15 @@ class RedirectIfUnverified {
     if ( $request->user()->hasConfirmation() )
     {
       flash()->warning('Ud. todavia no ha verificado su cuenta en el sistema.');
-      return redirect()->back();
+      return redirect('/por-verificar');
     }
+
+    if ( $request->user()->isDisabled() )
+    {
+      flash()->error('Ud. posee su cuenta desactivada, por favor contactenos a contacto@orbiagro.com.ve si considera que esto es un error.');
+      return redirect('/por-verificar');
+    }
+
     return $next($request);
   }
 
