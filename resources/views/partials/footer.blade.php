533ba9caf7a2627c51f4d42b92f1492044476fc8
slayerfat <slayerfat@gmail.com>
2015-07-15 17:34:45 -0430

    ajuste de footer.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,11 +1,15 @@
-<footer>
-  <p>
+<footer class="footer-center">
+  <p class="footer-orbiagro">
     orbiagro.com.ve
   </p>
 
-  <p>
-    <a href="#">test</a>
-    <a href="#">test</a>
-    <a href="#">test</a>
+  <p class="footer-enlaces">
+    {!! link_to_action('HomeController@index', 'Inicio') !!}
+    {!! link_to_action('MakersController@index', 'Fabricantes') !!}
+    {!! link_to_action('CategoriesController@index', 'Categorias') !!}
+    {!! link_to_action('SubCategoriesController@index', 'Rubros') !!}
+    {!! link_to_action('ProductsController@index', 'Productos') !!}
   </p>
+
+  <p class="footer-orbiagro">slayerfat © 2015</p>
 </footer>
