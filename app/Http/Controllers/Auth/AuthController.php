bfea4d6c9eeb190e20e2e9d90658042fe140bfdf
slayerfat <slayerfat@gmail.com>
2015-07-31 06:47:56 -0430

    fix authcontroller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -37,15 +37,10 @@ class AuthController extends Controller {
   /**
    * Create a new authentication controller instance.
    *
-   * @param  \Illuminate\Contracts\Auth\Guard  $auth
-   * @param  \Illuminate\Contracts\Auth\Registrar  $registrar
    * @return void
    */
   public function __construct()
   {
-    $this->auth = $auth;
-    $this->registrar = $registrar;
-
     $this->middleware('guest', ['except' => 'getLogout']);
   }
 
