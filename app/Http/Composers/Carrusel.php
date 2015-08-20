45c5e4376b62611f0bce92ca33ec5cb19a2d6b6f
slayerfat <slayerfat@gmail.com>
2015-08-20 17:50:46 -0430

    cambios iniciales para psr-2

--- app/Http/Composers/Carrusel.php
+++ app/Http/Composers/Carrusel.php
@@ -8,40 +8,41 @@ use App\PromoType;
 use App\Promotion;
 use App\SubCategory;
 
-class Carrusel {
+class Carrusel
+{
 
-  public function composeHomeCarruselImages(View $view)
-  {
-    $collection = collect();
+    public function composeHomeCarruselImages(View $view)
+    {
+        $collection = collect();
 
-    $products = Product::latest()->take(5)->get();
+        $products = Product::latest()->take(5)->get();
 
-    $typesId = PromoType::whereIn('description', ['primavera', 'verano', 'otoño', 'invierno'])->lists('id');
-    // selecciona las promociones existentes segun el tipo ya seleccionado
-    $promotions = Promotion::whereIn('promo_type_id', $typesId)->random()->take(3)->get();
+        $typesId = PromoType::whereIn('description', ['primavera', 'verano', 'otoño', 'invierno'])->lists('id');
+        // selecciona las promociones existentes segun el tipo ya seleccionado
+        $promotions = Promotion::whereIn('promo_type_id', $typesId)->random()->take(3)->get();
 
-    $item = Category::random()->first();
-    $item->controller = 'CategoriesController@show';
+        $item = Category::random()->first();
+        $item->controller = 'CategoriesController@show';
 
-    $collection->push($item);
+        $collection->push($item);
 
-    $item = SubCategory::has('products')->random()->first();
-    $item->controller = 'SubCategoriesController@show';
+        $item = SubCategory::has('products')->random()->first();
+        $item->controller = 'SubCategoriesController@show';
 
-    $collection->push($item);
+        $collection->push($item);
 
-    $products->each(function($product) use($collection){
-      $product->controller = 'ProductsController@show';
-      $collection->push($product);
-    });
+        $products->each(function($product) use($collection) {
+            $product->controller = 'ProductsController@show';
+            $collection->push($product);
+        });
 
-    $promotions->each(function($promo) use($collection){
-      $promo->controller = 'PromotionsController@show';
-      $collection->push($promo);
-    });
+        $promotions->each(function($promo) use($collection) {
+            $promo->controller = 'PromotionsController@show';
+            $collection->push($promo);
+        });
 
-    $collection = $collection->shuffle();
+        $collection = $collection->shuffle();
 
-    $view->with('carruselCollection', $collection);
-  }
+        $view->with('carruselCollection', $collection);
+    }
 }
--- app/Http/Controllers/CharacteristicsController.php
+++ app/Http/Controllers/CharacteristicsController.php
@@ -10,117 +10,121 @@ use App\Characteristic;
 
 use App\Mamarrachismo\ModelValidation;
 
-class CharacteristicsController extends Controller {
+class CharacteristicsController extends Controller
+{
 
-  private $user, $userId, $characteristic;
+    private $user;
 
-  /**
-   * Create a new controller instance.
-   *
-   * @method __construct
-   * @param  Feature     $feature
-   *
-   * @return void
-   */
-  public function __construct(Characteristic $characteristic, Guard $auth)
-  {
-    $this->user   = $auth->user();
+    private $userId;
 
-    // artisan route:list fail
-    $this->userId = $auth->user() ? $auth->user()->id : null;
+    private $characteristic;
 
-    $this->modelValidator = new ModelValidation($this->userId, $this->user);
-    $this->characteristic = $characteristic;
-  }
+    /**
+    * Create a new controller instance.
+    *
+    * @method __construct
+    * @param  Feature     $feature
+    *
+    * @return void
+    */
+    public function __construct(Characteristic $characteristic, Guard $auth)
+    {
+        $this->user   = $auth->user();
 
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return Response
-   */
-  public function create($id)
-  {
-    $product = Product::findOrFail($id)->load('mechanical');
+        // artisan route:list fail
+        $this->userId = $auth->user() ? $auth->user()->id : null;
 
-    if($this->modelValidator->notOwner($product->user->id)) :
-      flash()->error('Ud. no tiene permisos para esta accion.');
-      return redirect()->action('ProductsController@show', $product->slug);
-    endif;
-
-    if ($product->characteristics) {
-      flash()->error('Este Producto ya posee caracteristicas, por favor actualice las existentes.');
-      return redirect()->action('ProductsController@show', $product->slug);
+        $this->modelValidator = new ModelValidation($this->userId, $this->user);
+        $this->characteristic = $characteristic;
     }
 
-    return view('characteristic.create')->with([
-      'product' => $product,
-      'characteristic'    => $this->characteristic
-    ]);
-  }
+    /**
+    * Show the form for creating a new resource.
+    *
+    * @return Response
+    */
+    public function create($id)
+    {
+        $product = Product::findOrFail($id)->load('mechanical');
 
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @return Response
-   */
-  public function store($id, CharacteristicRequest $request)
-  {
-    $product = Product::findOrFail($id)->load('mechanical');
+        if ($this->modelValidator->notOwner($product->user->id)) {
+            flash()->error('Ud. no tiene permisos para esta accion.');
+            return redirect()->action('ProductsController@show', $product->slug);
+        }
 
-    if($this->modelValidator->notOwner($product->user->id)) :
-      flash()->error('Ud. no tiene permisos para esta accion.');
-      return redirect()->action('ProductsController@show', $product->slug);
-    endif;
+        if ($product->characteristics) {
+            flash()->error('Este Producto ya posee caracteristicas, por favor actualice las existentes.');
+            return redirect()->action('ProductsController@show', $product->slug);
+        }
 
-    if ($product->characteristic) :
-      flash()->error('Este Producto ya posee caracteristicas, por favor actualice las existentes.');
-      return redirect()->action('ProductsController@show', $product->slug);
-    endif;
+        return view('characteristic.create')->with([
+            'product'        => $product,
+            'characteristic' => $this->characteristic
+        ]);
+    }
 
-    $this->characteristic = new Characteristic($request->all());
-    $product->characteristics()->save($this->characteristic);
+    /**
+    * Store a newly created resource in storage.
+    *
+    * @return Response
+    */
+    public function store($id, CharacteristicRequest $request)
+    {
+        $product = Product::findOrFail($id)->load('mechanical');
 
-    flash('Caracteristicas del producto creadas exitosamente.');
-    return redirect()->action('ProductsController@show', $product->slug);
-  }
+        if ($this->modelValidator->notOwner($product->user->id)) {
+            flash()->error('Ud. no tiene permisos para esta accion.');
+            return redirect()->action('ProductsController@show', $product->slug);
+        }
 
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function edit($id)
-  {
-    $this->characteristic = Characteristic::findOrFail($id)->load('product');
+        if ($product->characteristic) :
+            flash()->error('Este Producto ya posee caracteristicas, por favor actualice las existentes.');
+            return redirect()->action('ProductsController@show', $product->slug);
+        endif;
 
-    if($this->modelValidator->notOwner($this->characteristic->product->user->id)) :
-      flash()->error('Ud. no tiene permisos para esta accion.');
-      return redirect()->action('ProductsController@show', $this->characteristic->product->slug);
-    endif;
+        $this->characteristic = new Characteristic($request->all());
+        $product->characteristics()->save($this->characteristic);
 
-    return view('characteristic.edit')->with(['characteristic' => $this->characteristic]);
-  }
+        flash('Caracteristicas del producto creadas exitosamente.');
+        return redirect()->action('ProductsController@show', $product->slug);
+    }
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function update($id, CharacteristicRequest $request)
-  {
-    $this->characteristic = Characteristic::findOrFail($id)->load('product');
+    /**
+    * Show the form for editing the specified resource.
+    *
+    * @param  int  $id
+    * @return Response
+    */
+    public function edit($id)
+    {
+        $this->characteristic = Characteristic::findOrFail($id)->load('product');
 
-    if($this->modelValidator->notOwner($this->characteristic->product->user->id)) :
-      flash()->error('Ud. no tiene permisos para esta accion.');
-      return redirect()->action('ProductsController@show', $this->characteristic->product->slug);
-    endif;
+        if ($this->modelValidator->notOwner($this->characteristic->product->user->id)) {
+            flash()->error('Ud. no tiene permisos para esta accion.');
+            return redirect()->action('ProductsController@show', $this->characteristic->product->slug);
+        }
 
-    $this->characteristic->update($request->all());
+        return view('characteristic.edit')->with(['characteristic' => $this->characteristic]);
+    }
 
-    flash('Caracteristicas del Producto Actualizadas exitosamente.');
-    return redirect()->action('ProductsController@show', $this->characteristic->product->slug);
-  }
+    /**
+    * Update the specified resource in storage.
+    *
+    * @param  int  $id
+    * @return Response
+    */
+    public function update($id, CharacteristicRequest $request)
+    {
+        $this->characteristic = Characteristic::findOrFail($id)->load('product');
 
+        if ($this->modelValidator->notOwner($this->characteristic->product->user->id)) {
+            flash()->error('Ud. no tiene permisos para esta accion.');
+            return redirect()->action('ProductsController@show', $this->characteristic->product->slug);
+        }
+
+        $this->characteristic->update($request->all());
+
+        flash('Caracteristicas del Producto Actualizadas exitosamente.');
+        return redirect()->action('ProductsController@show', $this->characteristic->product->slug);
+    }
 }
