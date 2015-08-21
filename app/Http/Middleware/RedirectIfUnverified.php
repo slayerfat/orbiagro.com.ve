7a0f5810617ae4d8ac5ec929c4466b31b5e22e13
slayerfat <slayerfat@gmail.com>
2015-08-21 10:29:36 -0430

    PSR-2 completo en Middlewares

--- app/Http/Middleware/RedirectIfUnverified.php
+++ app/Http/Middleware/RedirectIfUnverified.php
@@ -2,30 +2,28 @@
 
 use Closure;
 
-class RedirectIfUnverified {
+class RedirectIfUnverified
+{
 
-  /**
-   * Handle an incoming request.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  \Closure  $next
-   * @return mixed
-   */
-  public function handle($request, Closure $next)
-  {
-    if ( $request->user()->hasConfirmation() )
+    /**
+    * Handle an incoming request.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \Closure  $next
+    * @return mixed
+    */
+    public function handle($request, Closure $next)
     {
-      flash()->warning('Ud. todavia no ha verificado su cuenta en el sistema.');
-      return redirect('/por-verificar');
+        if ($request->user()->hasConfirmation()) {
+            flash()->warning('Ud. todavia no ha verificado su cuenta en el sistema.');
+            return redirect('/por-verificar');
+        }
+
+        if ($request->user()->isDisabled()) {
+            flash()->error('Cuenta desactivada, escribanos a contacto@orbiagro.com.ve si cree que es un error.');
+            return redirect('/por-verificar');
+        }
+
+        return $next($request);
     }
-
-    if ( $request->user()->isDisabled() )
-    {
-      flash()->error('Ud. posee su cuenta desactivada, por favor contactenos a contacto@orbiagro.com.ve si considera que esto es un error.');
-      return redirect('/por-verificar');
-    }
-
-    return $next($request);
-  }
-
 }
