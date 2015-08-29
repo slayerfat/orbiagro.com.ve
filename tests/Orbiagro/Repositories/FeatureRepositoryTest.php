c07ec38c946ffe92a6796d02280bd23fa8b8e426
slayerfat <slayerfat@gmail.com>
2015-08-28 19:32:49 -0430

    preparando test para update

--- tests/Orbiagro/Repositories/FeatureRepositoryTest.php
+++ tests/Orbiagro/Repositories/FeatureRepositoryTest.php
@@ -2,6 +2,7 @@
 
 use Mockery;
 use Orbiagro\Models\Feature;
+use Orbiagro\Models\Product;
 use Orbiagro\Repositories\FeatureRepository;
 use Tests\TestCase;
 
@@ -65,5 +66,62 @@ class FeatureRepositoryTest extends TestCase
             ->makePartial();
 
         $this->assertFalse($repo->validateCreateRequest(1));
+    }
+
+    public function testCreate()
+    {
+        $mock = Mockery::mock(Feature::class);
+
+        $productMock = Mockery::mock(Product::class)
+            ->makePartial();
+
+        $productMock->shouldReceive('features')
+            ->once()
+            ->andReturnSelf();
+
+        $productMock->shouldReceive('save')
+            ->once()
+            ->andReturnNull();
+
+        $repo = Mockery::mock(FeatureRepository::class, [$mock])
+            ->shouldAllowMockingProtectedMethods()
+            ->makePartial();
+
+        $repo->shouldReceive('getNewInstance')
+            ->once()
+            ->andReturn([]);
+
+        $this->assertEquals(
+            [],
+            $repo->create([], $productMock)
+        );
+    }
+
+    public function testUpdate()
+    {
+        $mock = Mockery::mock(Feature::class);
+
+        $repo = Mockery::mock(FeatureRepository::class, [$mock])
+            ->shouldAllowMockingProtectedMethods()
+            ->makePartial();
+
+        $repo->shouldReceive('getById')
+            ->once()
+            ->with(1)
+            ->andReturnSelf();
+
+        $repo->shouldReceive('update')
+            ->once()
+            ->andReturnNull();
+
+        $repo->shouldReceive('load')
+            ->once()
+            ->andReturnNull();
+
+        $this->markTestIncomplete('Lluvia seria, pushing antes que se vaya la luz, lol');
+
+        $this->assertNull(
+            $repo->update(1, [])
+        );
     }
 }
