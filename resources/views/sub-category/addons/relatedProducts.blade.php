d2a08ec3f1edd68695a5fdd941526dc1cbd5be5f
slayerfat <slayerfat@gmail.com>
2015-09-04 15:56:29 -0430

    ajustada logica dentro del repositorio de categorias.
    error casual en la devolucion de colecciones de productos.
    
    bug en productos y rubros para ser mostrados en index, corregidos.

--- resources/views/sub-category/addons/relatedProducts.blade.php
+++ resources/views/sub-category/addons/relatedProducts.blade.php
@@ -1,13 +1,15 @@
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12">
-      <h1>{!!$title!!}</h1>
+@unless($subCategory->products->isEmpty())
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <h1>{!!$title!!}</h1>
+      </div>
+    </div>
+    <div class="row">
+      @include('partials.products.gallerie-thumbnail-products', [
+        'products' => $subCategory->products->take(3),
+        'size' => 'col-sm-4'
+      ])
     </div>
   </div>
-  <div class="row">
-    @include('partials.products.gallerie-thumbnail-products', [
-      'products' => $subCategory->products->take(3),
-      'size' => 'col-sm-4'
-    ])
-  </div>
-</div>
+@endunless
